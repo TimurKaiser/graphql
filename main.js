f00f1282443e892ce3b2c680b7fdb8196e2f337a
baksoy <baran.aksoy76800@gmail.com>
2024-08-29 14:05:06 +0200

    token and fetch ready

---
+++ main.js
@@ -0,0 +1,84 @@
+import { getToken } from "./module.js"
+
+
+window.addEventListener('tokenReady', (event) => {
+  const token = getToken();
+  console.log("Test", token)
+})
+
+//const url = "https://zone01normandie.org/api/graphql-engine/v1/graphql"
+
+//const queryData = `{
+//    user {
+//      auditRatio
+//      firstName
+//      lastName
+//    }
+//    event_user {
+//      event {
+//        path
+//      }
+//      userLogin
+//      level
+//    }
+//}`;
+
+
+
+//const queryData = `{
+//  user {
+//    auditRatio
+//    firstName
+//    lastName
+//  }
+//  event_user {
+//    userLogin
+//    level
+//  }
+//}`
+
+//const queryData = `{
+//{
+//  user {
+//    auditRatio
+//    firstName
+//    lastName
+//    xps {
+//      amount
+//      path
+//    }
+//  }
+//
+//
+//  event_user {
+//    userLogin
+//    level
+//    xp {
+//      amount 
+//    }
+//    event {
+//      path
+//    }
+//  }
+//}
+//}`
+//
+//fetch(url, {
+//    method:"POST",
+//    headers: {
+//        "Content-Type": "application/json",
+//        //Bearer est utilisé car c'est un token et nom user+mdp
+//        "Authorization": `Bearer ${token}`
+//    },  
+//    body: JSON.stringify({query: queryData})
+//})
+//
+//.then(response => response.json())
+//.then(data => {
+//    console.log(JSON.stringify(data))
+//})
+//
+//.catch(error => {
+//    console.log(error)
+//})
+//
---
+++ token/sign.js
@@ -0,0 +1,32 @@
+import { setToken } from "../module.js";
+
+const url = "https://zone01normandie.org/api/auth/signin";
+const username = "";
+const password = "";
+
+const credentials = btoa(`${username}:${password}`)
+
+const tokenQuery = ``;
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    // bearer or basic auth
+    "Authorization": `Basic ${credentials}`
+  },
+  body: JSON.stringify({ query: tokenQuery })
+})
+
+.then(response => response.json())
+.then(token => {
+  console.log(JSON.stringify(token));
+  setToken(token);
+
+  const event = new CustomEvent('tokenReady', {detail: token});
+  window.dispatchEvent(event);
+})
+
+.catch(error => {
+  console.error('Erreur dans sign:', error);
+});
