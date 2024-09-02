6843abec7cd7fd9bb9bc443eb7e7909ccd9299cf
baksoy <baran.aksoy76800@gmail.com>
2024-09-02 14:05:52 +0200

    MIME Problems solved

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ http.createServer((req, res) => {
     const extname = path.extname(filePath);
     let contentType = 'text/html';
 
+    // gestion du bon type MIME
     switch (extname) {
         case '.js':
             contentType = 'application/javascript';
@@ -16,7 +17,6 @@ http.createServer((req, res) => {
         case '.css':
             contentType = 'text/css';
             break;
-        // Ajoutez d'autres types MIME si nécessaire
     }
 
     fs.readFile(filePath, (err, content) => {
@@ -31,3 +31,12 @@ http.createServer((req, res) => {
 }).listen(port, () => {
     console.log(`Serveur running at http://localhost:${port}`);
 });
+
+
+
+
+//Ce code gère les fichiers avec les bons types MIME. 
+//MIME indique au navigateur comment interpréter les fichiers (ex : application/javascript pour les JS). 
+//Cela résout les problèmes de chargement des scripts et garantit 
+//leur traitement correct. Sans les bons types MIME, les navigateurs 
+//peuvent bloquer ou mal interpréter les fichiers, causant des erreurs.
