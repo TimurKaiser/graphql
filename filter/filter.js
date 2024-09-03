687df21eb038a71db259b58dbe89b1ce7e90d9e8
baksoy <baran.aksoy76800@gmail.com>
2024-09-03 11:31:37 +0200

    skills are filtered

--- filter/filter.js
+++ filter/filter.js
@@ -1,23 +1,31 @@
 window.addEventListener('dataReady', (event) => {
-    const getData = sessionStorage.getItem('myData')
+    const getData = sessionStorage.getItem('myData');
+    
     if (getData) {
-        // convertir la chaine en json pour plus de manipulation
+        // Convertir la chaîne en JSON pour plus de manipulation
         const myData = JSON.parse(getData);
         const transactions = myData.data.transaction;
-        const skillTransaction = transactions.filter(transaction => transaction.type.includes('skill_'))
+
+        const skillTransaction = transactions.filter(transaction => transaction.type.includes('skill_'));
 
         console.log(skillTransaction);
 
+        // si le tableau acc pour le type skill.type n'existe pas le créé
+        // et push tout les skill dedans
+        // reduce vas aller parcourir et push implémenter
+        const groupedSkills = skillTransaction.reduce((acc, skill) => {
+            if (!acc[skill.type]) {
+                acc[skill.type] = [];
+            }
+            
+            acc[skill.type].push(skill);
+            
+            return acc;
+        }, {});
 
+        console.log(groupedSkills);
+        
     } else {
-        console.log("No data in filter")
+        console.log("No data in filter");
     }
-
-
 });
-
-
-
-
-
-
